ff63142e6d95b57f56208a0a890e216f02814c49
Rick Lan <ricklan@gmail.com>
2025-03-31 09:47:34 +0800

    better filter (?)

--- opendbc/car/radar_interface.py
+++ opendbc/car/radar_interface.py
@@ -5,27 +5,28 @@
 #
 # - The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 # - Commercial use (e.g., use in a product, service, or activity intended to generate revenue) is prohibited without explicit written permission from Rick Lan. Contact ricklan@gmail.com for inquiries.
+# - Any project that uses the Software must visibly mention the following acknowledgment: "This project uses software from Rick Lan and is licensed under a custom license requiring permission for use."
 #
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 from opendbc.car.interfaces import RadarInterfaceBase
 from opendbc.can.parser import CANParser
 from opendbc.car.structs import RadarData
-from typing import List, Tuple, Dict, Optional
+from typing import List, Tuple, Optional, Dict
 
-BUS = 1
-
-class RadarConfig:
-    DREL_OFFSET = -1.2 # in meters
-    MAX_TRACKING_OBJS = 50
+DREL_OFFSET = -1.3 # car head to radar
+MAX_OBJECTS = 50
+MAX_LAT_DIST = 1.2 # lat distance
+MIN_LON_DIST = 1.5 # long distance
+FREQ = 5 # 20 hz
 
 def _create_radar_parser():
-    messages = [("Status", 16.7), ("ObjectData", 0)]
-    return CANParser('u_radar', messages, BUS)
+    messages = [("ObjectData", 0)]
+    return CANParser('u_radar', messages, 1)
 
 def _create_radar_object_parser():
-    messages = [(f"ObjectData_{i}", 0) for i in range(RadarConfig.MAX_TRACKING_OBJS)]
-    return CANParser('u_radar', messages, BUS)
+    messages = [(f"ObjectData_{i}", 0) for i in range(MAX_OBJECTS)]
+    return CANParser('u_radar', messages, 1)
 
 
 class RadarInterface(RadarInterfaceBase):
@@ -33,7 +34,9 @@ class RadarInterface(RadarInterfaceBase):
         super().__init__(CP)
 
         self.updated_messages = set()
+        self.objs: dict[int, dict] = {}
 
+        self._radar_points_cache = {}
         self.rcp = _create_radar_parser()
         self.rop = _create_radar_object_parser()
 
@@ -47,76 +50,88 @@ class RadarInterface(RadarInterfaceBase):
         new_list_append = new_list.append  # Local reference for faster access
 
         records = can_strings[0][1]
-        count = 0
+        id_num = 1
 
         for record in records:
-            if count >= RadarConfig.MAX_TRACKING_OBJS:
+            if id_num > MAX_OBJECTS:
                 break
 
             if record[0] == 0x60B:
-                new_list_append((count + 383, record[1], record[2]))
-                count += 1
+                new_list_append((id_num + 383, record[1], record[2]))
+                id_num += 1
 
         return [(can_strings[0][0], new_list)], len(new_list)
 
-    def _is_valid_track(self, obj_d_rel: float, obj_class: int, obj_rcs: float, obj_movement: int) -> bool:
-        return (obj_d_rel > 0. and
-                (obj_class == 1 or obj_rcs > 0.) and
-                obj_movement != 2)
+    def _is_valid_track(self, obj: Dict) -> bool:
+        return (obj['d_rel'] > MIN_LON_DIST and
+                abs(obj['y_rel']) < MAX_LAT_DIST and
+                (obj['class'] == 1 or obj['rcs'] > 0.) and
+                obj['movement'] != 2)
 
-    def update(self, can_strings: List[Tuple]) -> Optional[RadarData]:
+    def _update_pts(self, track_id: int, obj: Dict) -> None:
+        if self._is_valid_track(obj):
+            if track_id not in self.pts:
+                if track_id in self._radar_points_cache:
+                    self.pts[track_id] = self._radar_points_cache[track_id]
+                else:
+                    self.pts[track_id] = RadarData.RadarPoint()
+                    self.pts[track_id].trackId = track_id
+                    self._radar_points_cache[track_id] = self.pts[track_id]
+
+            point = self.pts[track_id]
+            point.yvRel = obj['yv_rel']
+            point.dRel = obj['d_rel']
+            point.yRel = obj['y_rel']
+            point.vRel = obj['v_rel']
+            point.aRel = float('nan')
+
+        elif track_id in self.pts:
+            del self.pts[track_id]
+
+    def update(self, can_strings):
         if self.rcp is None:
             return super().update(None)
 
+        self.frame += 1
         vls = self.rcp.update_strings(can_strings)
         self.updated_messages.update(vls)
 
         # update radar points
         if 0x60B in self.updated_messages:
-            parsable_can_string, obj_count = self._create_parsable_object_can_strings(can_strings)
+
+            # parse objects, stored into objs array
+            parsable_can_string, size = self._create_parsable_object_can_strings(can_strings)
             self.rop.update_strings(parsable_can_string)
 
-            for i in range(obj_count):
+            # Batch update objects
+            for i in range(size):
                 cpt = self.rop.vl[f'ObjectData_{i}']
 
-                # Update object properties in batch
                 track_id = int(cpt['ID'])
-                obj_class = int(cpt['Class'])
-                obj_rcs = float(cpt['RCS'])
-                obj_movement = int(cpt['DynProp'])
-                obj_yv_rel = float(cpt['VRelLat'])
-                obj_d_rel = float(cpt['DistLong']) + RadarConfig.DREL_OFFSET
-                obj_y_rel = -float(cpt['DistLat'])
-                obj_v_rel = float(cpt['VRelLong'])
+                if track_id not in self.objs:
+                    self.objs[track_id] = {}
 
-                # skip invalid objects
-                if not self._is_valid_track(obj_d_rel, obj_class, obj_rcs, obj_movement):
-                    if track_id in self.pts:
-                        del self.pts[track_id]
-                    continue
-
-                if track_id not in self.pts:
-                    self.pts[track_id] = RadarData.RadarPoint()
-                    self.pts[track_id].trackId = track_id
-
-                # update points
-                point = self.pts[track_id]
-                point.yvRel = obj_yv_rel
-                point.dRel = obj_d_rel
-                point.yRel = obj_y_rel
-                point.vRel = obj_v_rel
-                point.aRel = float('nan')
-
-        # dispatch
-        if 0x60A in self.updated_messages:
-            ret = RadarData()
-
-            ret.points = list(self.pts.values()) if self.pts else []
-
-            self.updated_messages.clear()
-            self.pts.clear()
-
-            return ret
+                self.objs[track_id] = {
+                    'track_id': track_id,
+                    'class': int(cpt['Class']),
+                    'rcs': float(cpt['RCS']),
+                    'movement': int(cpt['DynProp']),
+                    'yv_rel': float(cpt['VRelLat']),
+                    'd_rel': float(cpt['DistLong']) + DREL_OFFSET,
+                    'y_rel': -float(cpt['DistLat']),
+                    'v_rel': float(cpt['VRelLong'])
+                }
 
         self.updated_messages.clear()
+
+        # dispatch
+        if self.frame % FREQ == 0:
+            # update self.pts before dispatch
+            for track_id, obj in self.objs.items():
+                self._update_pts(track_id, obj)
+
+            ret = RadarData()
+            ret.points = list(self.pts.values()) if self.pts else []
+            return ret
+
         return None
